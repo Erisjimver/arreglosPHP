ddacb63c1df8992ab5f76a217a76b513c9316b5d
erisjimver <erisjinver@gmail.com>
2019-08-07 23:08:14 -0500

    recorrer array multidimencionales

--- array.php
+++ array.php
@@ -7,7 +7,7 @@
 
 <?php
 
-	$alimentos=array("fruta"=>array("kiwi",
+	$alimentos=array("fruta"=>array("tropical" =>"kiwi",
 					 				"citrico"=>"mandarina",
 					 				"otros"=>"manzana"),
 					"leche"=>array("animal"=>"vaca",
@@ -15,9 +15,29 @@
 					"carne"=>array("vacuno"=>"lomo",
 									"porcino"=>"pata"));
 
-	echo "El array contiene: " . $alimentos["carne"]["vacuno"];
+	//echo "El array contiene: " . $alimentos["carne"]["vacuno"];
+/*
+	foreach($alimentos as $clave_alim=>$alim){
 
+		echo "$clave_alim: <br>";
 
+		while(list($clave,$valor)=each($alim)){
+
+			echo "$clave = $valor <br>";
+
+		}
+		echo "<br>";
+	}
+*/
+	foreach ($alimentos as $clave_alim=> $alim) {
+    echo "$clave_alim: <br>"; 
+
+ 
+     foreach ($alim as $clave=> $valor) {
+      echo "$clave: $valor <br>"; 
+  	}
+  	echo "<br>";
+    }
 
 ?>
 
