5116db0acb81b365bbaf47dca354d9259b4da5b1
erisjimver <erisjinver@gmail.com>
2019-08-07 21:01:17 -0500

    first commit

---
+++ array.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Array Asociativo</title>
+</head>
+<body>
+
+<?php
+
+	$alimentos=array("fruta"=>array("kiwi",
+					 				"citrico"=>"mandarina",
+					 				"otros"=>"manzana"),
+					"leche"=>array("animal"=>"vaca",
+									"vegetal"=>"coco"),
+					"carne"=>array("vacuno"=>"lomo",
+									"porcino"=>"pata"));
+
+	echo "El array contiene: " . $alimentos["carne"]["vacuno"];
+
+
+
+
+?>
+
+</body>
+</html>
